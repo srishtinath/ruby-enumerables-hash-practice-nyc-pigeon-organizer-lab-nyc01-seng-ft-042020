035a3fbcfc4f6eda8b905037a47a01ea7cab1600
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 17:50:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.reduce({}) do | memo, (attribute, attribute_and_name)|
@@ -9,5 +29,7 @@ def nyc_pigeon_organizer(data)
           inside_hash[attribute] = attribute_value
           outer_hash[name] = inner_hash
         end
-        
+
 end
+
+nyc_pigeon_organizer(pigeon_data)
