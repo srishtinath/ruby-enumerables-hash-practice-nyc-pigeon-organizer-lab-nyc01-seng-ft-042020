17a3c54c28c1f6170b9d56ee56d554a5ce301e3c
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 18:12:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ def nyc_pigeon_organizer(data)
   data.reduce({}) do | memo_name, (attribute, attribute_and_name)|
     memo_name = {}
         inner_hash = attribute_and_name.reduce({}) do |memo, (attribute_value, name)|
-          inner_array = attribute_and_name.map {|attribute_value, name| attribute_value.to_s}
+          inner_array = attribute_and_name.map { |attribute_value, name| attribute_value.to_s}
           memo[attribute] = inner_array
     memo_name[name] = inner_hash
       end
