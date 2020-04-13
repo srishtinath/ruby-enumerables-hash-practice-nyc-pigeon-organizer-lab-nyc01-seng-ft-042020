7fe2264e8c59d8b06dcf4fac745be2dc204e381d
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 18:24:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, value|
+     value.each do |stats, name_array|
+        name_array.each do |name|
+          if pigeon_list[name] == nil
+            pigeon_list[name] = {}
+          end
+          if pigeon_list[name][attribute] == nil
+            pigeon_list[name][attribute] = []
+          end
+          pigeon_list[name][attribute].push(stats.to_s)
+        end
+      end
+    end
+    pigeon_list
 end
