872c71da372df9cf7b37dea2e219340034cce9fa
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 17:48:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.reduce({}) do | memo, (attribute, attribute_and_name)|
+
+    inner_array = []
+    outer_hash = {}
+    inner_hash = attribute_and_name.reduce({}) do | inside_hash, (attribute_value, name)|
+          inner_array << attribute_value.to_s
+          inside_hash[attribute] = attribute_value
+          outer_hash[name] = inner_hash
+        end
+        
 end
